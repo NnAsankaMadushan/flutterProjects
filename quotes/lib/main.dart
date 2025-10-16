9eaaf395b62bb47984f96ff3890b8c54e4aa350f
Jaxon <nnamadushan@gmail.com>
2025-10-16 17:58:00 +0530

    feat: cards

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -19,6 +19,36 @@ class _QuotesListState extends State<QuotesList> {
     Quote("A room without books is like a body without a soul.", "Marcus Tullius Cicero"),
   ];
 
+  Widget quoteTemplate (quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                '${quote.text}',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.grey[800],
+                ),
+              ),
+              SizedBox(height: 6.0),
+              Text(
+                '${quote.author}',
+                style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.grey[600],
+                ),
+                textAlign: TextAlign.right,
+              ),
+            ],
+          ),
+      ),
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +64,7 @@ class _QuotesListState extends State<QuotesList> {
         centerTitle: true,
         backgroundColor: Colors.redAccent,
       ),
-      body: Column(children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList()),
+      body: Column(children: quotes.map((quote) => quoteTemplate(quote)).toList()),
     );
   }
 }
