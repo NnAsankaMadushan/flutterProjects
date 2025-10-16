0b96685278c00bcb06f7a157d21ddf117f6fa55d
Jaxon <nnamadushan@gmail.com>
2025-10-16 17:27:46 +0530

    quotes project

---
+++ quotes/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: QuotesList()));
+}
+
+class QuotesList extends StatefulWidget {
+  const QuotesList({super.key});
+
+  @override
+  State<QuotesList> createState() => _QuotesListState();
+}
+
+class _QuotesListState extends State<QuotesList> {
+  List<Quote> quotes = [
+    Quote("Be yourself; everyone else is already taken.", "Oscar Wilde"),
+    Quote("So many books, so little time.", "Frank Zappa"),
+    Quote("A room without books is like a body without a soul.", "Marcus Tullius Cicero"),
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("Awesome Quotes", style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 24.0,
+          letterSpacing: 2.0,
+        ),),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList()),
+    );
+  }
+}
