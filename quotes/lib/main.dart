630626b382aafba2245012ffb5f27bbfd32a195c
Jaxon <nnamadushan@gmail.com>
2025-10-17 15:01:59 +0530

    fixes

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -1,70 +1,44 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
-void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: QuotesList()));
-}
+void main() => runApp(
+  const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: QuoteList(),
+  ),
+);
 
-class QuotesList extends StatefulWidget {
-  const QuotesList({super.key});
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
 
   @override
-  State<QuotesList> createState() => _QuotesListState();
+  State<QuoteList> createState() => _QuoteListState();
 }
 
-class _QuotesListState extends State<QuotesList> {
-  List<Quote> quotes = [
-    Quote("Be yourself; everyone else is already taken.", "Oscar Wilde"),
-    Quote("So many books, so little time.", "Frank Zappa"),
-    Quote("A room without books is like a body without a soul.", "Marcus Tullius Cicero"),
+class _QuoteListState extends State<QuoteList> {
+  final List<Quote> quotes = [
+    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
+    Quote(author: 'Oscar Wilde', text: 'I have nothing to declare except my genius'),
+    Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple'),
   ];
-
-  Widget quoteTemplate (quote) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                '${quote.text}',
-                style: TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.grey[800],
-                ),
-              ),
-              SizedBox(height: 6.0),
-              Text(
-                '${quote.author}',
-                style: TextStyle(
-                  fontSize: 14.0,
-                  color: Colors.grey[600],
-                ),
-                textAlign: TextAlign.right,
-              ),
-            ],
-          ),
-      ),
-    );
-  }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text("Awesome Quotes", style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-          fontSize: 24.0,
-          letterSpacing: 2.0,
-        ),),
+        title: const Text('Awesome Quotes'),
         centerTitle: true,
         backgroundColor: Colors.redAccent,
       ),
-      body: Column(children: quotes.map((quote) => quoteTemplate(quote)).toList()),
+      body: SingleChildScrollView( // added to make it scrollable
+        child: Column(
+          children: quotes
+              .map((quote) => QuoteCard(quote: quote))
+              .toList(),
+        ),
+      ),
     );
   }
 }
