ed53e0bde9de73d20d9cfc4f84e36f27b905a574
Asanka <nnamadushan@gmail.com>
2025-10-27 13:03:28 +0530

    init

---
+++ world_time/lib/pages/choose_location.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({super.key});
+
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  int count = 0;
+  @override
+  void initState() {
+    print("Inite state run");
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("Build state run");
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text("Choose a Location", style: TextStyle(
+          color: Colors.white,
+        ),),
+        centerTitle: true,
+        elevation: 0,
+        iconTheme: IconThemeData(
+          color: Colors.white, 
+        ),
+      ),
+      body: ElevatedButton(
+        onPressed: (){
+          setState(() {
+            count =count + 1;
+          });
+        },
+        child: Text("Count is : $count"),
+        )
+    );
+  }
+}
