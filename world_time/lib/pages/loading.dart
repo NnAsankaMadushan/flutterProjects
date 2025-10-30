ede7c04973a81a2b20f0476d19334c9ea2e6d550
Asanka Madushan <85320753+NnAsankaMadushan@users.noreply.github.com>
2025-10-30 14:22:41 +0530

    Update loading.dart

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter/material.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -10,36 +10,24 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
-    Uri uri = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
-    final response = await http.get(
-    uri,
-    headers: {
-      'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-      'Accept': 'application/json',
-    },
-  );
-    if (response.statusCode == 200) {
-    // ✅ Safe to decode JSON
+  void getTime() async {
+    // make the request
+    http.Response response = await http.get(Uri.parse('https://timeapi.io/api/time/current/zone?timeZone=Europe%2FLondon'));
     Map data = jsonDecode(response.body);
     print(data);
-    print(data['title']);
-  } else {
-    // ❌ Something went wrong
-    print('Request failed with status: ${response.statusCode}');
-    print('Response body: ${response.body}');
-  }
+    print(data['dateTime']);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Text('loading screen'),
+    ); // Scaffold
   }
 }
