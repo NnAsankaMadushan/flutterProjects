ae1509eb1c42a3c9580ddc123e9a79d31af3b4be
Jaxon <nnamadushan@gmail.com>
2025-11-06 17:28:49 +0530

    init

--- meals/lib/categories.dart
+++ meals/lib/categories.dart
@@ -6,6 +6,10 @@ import 'package:meals/widgets/category_grid_item.dart';
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
+  void _selectCategory() {
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +20,7 @@ class CategoriesScreen extends StatelessWidget {
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(24.0),
         child: GridView(
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
