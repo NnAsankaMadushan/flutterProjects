dee2e8871d2e519bf5e7b5839821887e17fecf5b
Jaxon <nnamadushan@gmail.com>
2025-11-10 14:31:11 +0530

    lessons

---
+++ lessons/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 98),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            )
+          ),
+          child: const Center(
+            child: Text(
+              "Unus Wallet",
+              style: TextStyle(
+                fontSize: 28,
+                color: Colors.white,
+              ),
+            )
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build (BuildContext context){
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: colors[
+          Color.alphaBlend(, background)
+        ])
+      ),
+    )
+  }
+}
