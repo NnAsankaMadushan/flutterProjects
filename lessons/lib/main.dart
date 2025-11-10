99ef9ac71500c6653f88e433d1eba19f3320c4d5
Jaxon <nnamadushan@gmail.com>
2025-11-10 15:42:37 +0530

    custom widgets

--- lessons/lib/main.dart
+++ lessons/lib/main.dart
@@ -1,37 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:lessons/gradient_container.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 26, 2, 80),
-                Color.fromARGB(255, 45, 7, 98),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              "Unus Wallet",
-              style: TextStyle(fontSize: 28, color: Colors.white),
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(body: GradientContainer())));
 }
 
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
