b74484c017cce1da77e013a1bbc41cded517e535
Jaxon <nnamadushan@gmail.com>
2025-11-11 13:16:36 +0530

    remove meterialApp from main

--- adv_basics/lib/main.dart
+++ adv_basics/lib/main.dart
@@ -1,14 +1,10 @@
-import 'package:adv_basics/gradient_container.dart';
+// import 'package:adv_basics/gradient_container.dart';
+import 'package:adv_basics/quiz.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: GradientContainer.blue(),
-      )
-    )
+    const Quiz()
   );
 }
 
