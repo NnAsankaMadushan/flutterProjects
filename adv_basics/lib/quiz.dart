24f661b62edb169f8996564f3be8e4f0e9ceb1b4
Jaxon <nnamadushan@gmail.com>
2025-11-12 10:38:16 +0530

    init

--- adv_basics/lib/quiz.dart
+++ adv_basics/lib/quiz.dart
@@ -1,3 +1,4 @@
+import 'package:adv_basics/questions_screen.dart';
 import 'package:adv_basics/start_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -9,18 +10,29 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionScreen();
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 78, 13, 151),
-              Color.fromARGB(255, 107, 15, 168),
-            ])
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+            ),
           ),
-         child:  const StartScreen(),
+          child: activeScreen,
         ),
       ),
     );
--- adv_basics/lib/start_screen.dart
+++ adv_basics/lib/start_screen.dart
@@ -5,26 +5,37 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Image.asset('assets/images/quiz-logo.png', width: 300,
-        color: const Color.fromARGB(150, 255, 255, 255),
-      ),
-        SizedBox(height: 30),
-        OutlinedButton.icon(
-          onPressed: (){
-            
-          },
-          style: OutlinedButton.styleFrom(
-            foregroundColor: Colors.white,
-            textStyle: const TextStyle(fontSize: 18),
-            padding: const EdgeInsets.all(20),
+    return Scaffold(
+      backgroundColor: Colors.purple,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/quiz-logo.png',
+                width: 200,
+                color: const Color.fromARGB(150, 255, 255, 255),
+              ),
+              const SizedBox(height: 50),
+              OutlinedButton.icon(
+                onPressed: () {
+                  
+                },
+                style: OutlinedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(fontSize: 18),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 30, vertical: 15),
+                  side: const BorderSide(color: Colors.white),
+                ),
+                icon: const Icon(Icons.arrow_right_alt),
+                label: const Text("Start Quiz"),
+              ),
+            ],
           ),
-          icon: const Icon(Icons.arrow_right_alt),
-          label: const Text("Start Quiz"),
         ),
-      ],
+      ),
     );
   }
 }
